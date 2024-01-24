25ffcc509440c4719c20d9ce64c82aebd7f64517
Kucera1 <156932274+Kucera1@users.noreply.github.com>
2024-01-24 11:17:10 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String jmenoPrijmeni = "Jaroslav Kučera";
+        BigDecimal datumNarozeniProdejce = BigDecimal.valueOf(1992-9-4);
+        int pocetDosudSjednanychSmluv = 10;
+        int celkoveMnozstviProdaneMrkveVTunach = 50;
+        String nazevMestaKdeProdejceSidli = "Praha";
+        String registracniZnackaVozidla = "3E3 1234";
+        String ipAdresaVerzeCtyriFiremnihoPocitace = "0.0.0.0/8";
+
+        float prumerProdaneMrkveNaJednuSmlouvy = celkoveMnozstviProdaneMrkveVTunach/pocetDosudSjednanychSmluv;
+        System.out.println("Průměr prodané mrkve na jednu smlouvu je: " +prumerProdaneMrkveNaJednuSmlouvy);
+
+
+
+
     }
 }
